c2ef71d85a91adb8c1966c4a21d754aaaea2c750
Your NameProgrammingm <niamulhossen223@gmail.com>
2026-01-04 01:40:22 +0600

    আপনার commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,77 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Person></Person>
+    <Person></Person>
+    <Test></Test>
+    <Ul> </Ul>
+    <Car></Car>
+    <Student name="Niamul" age={25}></Student>
+    <Student name="Rahim" age={26}></Student>
+    <Student name="Karim" age={27}></Student>
+     </>
+  )
+}
+
+function Person(){
+  return (
+    <p>niamul is engineer</p>
+  )
+}
+
+function Test(){
+  const age = 30;
+  return(
+    <p>{age}</p>
+  )
+}
+
+function Ul(){
+  const name= "niamul";
+  return(
+    <ul>
+      <li>{name}</li>
+    </ul>
+  )
+}
+
+function Car(){
+  const carName = "toyota";
+  const carModel = "2020";
+  const carColor = "red"; 
+  const carPrice = "50000$";
+  return(
+    <div>
+      <h3>Car Component</h3>
+      <p>Car Name: {carName}</p>
+      <p>Car Model: {carModel}</p>
+      <p>Car Color: {carColor}</p>
+      <p>Car Price: {carPrice}</p>
+    </div>
+  )
+}
+
+function Student(Props){
+  console.log(Props);
+  return(
+    <div style={{
+      border: '2px solid',
+      borderRadius: '20px',
+      margin: '10px',
+      padding: '10px'
+    }}>
+      <h3>Student Component</h3>
+      <p>Name: {Props.name}</p>
+      <p>Age: {Props.age}</p>
+    </div>
   )
 }
 
 export default App
+
+
